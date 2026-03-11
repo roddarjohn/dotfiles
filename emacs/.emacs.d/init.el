b5d097f78fd170c77d350ad72b16a04c520c28e9
Rodda John <johnrod.john@gmail.com>
2026-03-11 09:45:36 -0400

    .

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,37 +1,5 @@
-(require 'package)
-
-(setq package-enable-at-startup nil)
-(setq package-archives
-      '(("melpa" . "http://melpa.org/packages/")
-	("gnu" . "http://elpa.gnu.org/packages/")
-	("org" . "http://orgmode.org/elpa/")))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/init.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ace-window all-the-icons-ibuffer apheleia beacon cape casual-suite
-		company consult-projectile corfu devdocs
-		embark-consult expand-region flatland-theme
-		flycheck-ledger hungry-delete iedit ledger-mode
-		lsp-pyright magit marginalia orderless org-modern
-		org-super-agenda pyenv-mode quelpa-use-package
-		rainbow-delimiters terraform-mode transient
-		tree-sitter-langs tsi typescript-mode vertico
-		which-key yasnippet-snippets))
- '(warning-suppress-log-types '((defvaralias losing-value git-commit-mode-hook))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'ob-tangle)
+(let* ((init-org (file-truename (expand-file-name "init.org" user-emacs-directory)))
+       (config-el (expand-file-name "emacs-config.el" (file-name-directory init-org))))
+  (org-babel-tangle-file init-org)
+  (load config-el))
